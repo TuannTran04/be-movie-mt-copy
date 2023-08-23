ef68f8aa8495d146efa3c2518df727222611cf41
KienQuan <hotrankienquan144@gmail.com>
2023-08-23 10:27:27 +0700

    update otp, use otp firebase later

--- src/api/v1/controllers/uploadController.js
+++ src/api/v1/controllers/uploadController.js
@@ -3,103 +3,112 @@
 // import multer from "multer";
 // import config from "../../../config/firebase.config";
 
-const express = require('express')
-const {initializeApp} = require("firebase/app")
-const {getStorage, ref, getDownloadURL, uploadBytesResumable } = require("firebase/storage")
-const multer = require("multer")
-const config = require("../../../config/firebase.config")
-
-
+const express = require("express");
+const { initializeApp } = require("firebase/app");
+const { getAnalytics } = require("firebase/analytics");
+const {
+  getStorage,
+  ref,
+  getDownloadURL,
+  uploadBytesResumable,
+} = require("firebase/storage");
+const multer = require("multer");
+const config = require("../../../config/firebase.config");
+// import { getAnalytics } from "firebase/analytics";
 const router = express.Router();
 
-
 //Initialize a firebase application
-initializeApp(config.firebaseConfig);
-
+// initializeApp(config.firebaseConfig);
+const app = initializeApp(config.firebaseConfig);
+// const analytics = getAnalytics(app);
 // Initialize Cloud Storage and get a reference to the service
 const storage = getStorage();
 
 // Setting up multer as a middleware to grab photo uploads
 const upload = multer({ storage: multer.memoryStorage() });
-router.post('/photos/upload', upload.array('photos', 12), async function(
-  req,
-  res,
-  next
-) {
-    
-   try {
-    // console.log("check req multiple",req.files)
-    // console.log(req.body)
-  // req.files là một mảng của các file `photos`
-  // req.body sẽ giữ thông tin gắn kèm (vd: text fields), nếu có
-        const dateTime = giveCurrentDateTime();
-        let result = [
+router.post(
+  "/photos/upload",
+  upload.array("photos", 30),
+  async function (req, res, next) {
+    try {
+      // req.files là một mảng của các file `photos`
+      // req.body sẽ giữ thông tin gắn kèm (vd: text fields), nếu có
+      const dateTime = giveCurrentDateTime();
+      let result = [];
+      for (const file of req.files) {
+        const storageRef = ref(
+          storage,
+          `many_img/${file.originalname + "       " + dateTime}`
+        );
+        const metadata = {
+          contentType: file.mimetype,
+        };
+        const snapshot = await uploadBytesResumable(
+          storageRef,
+          file.buffer,
+          metadata
+        );
+        const downloadURL = await getDownloadURL(snapshot.ref);
+        result.push({
+          name: file.originalname,
+          type: file.mimetype,
+          downloadURL: downloadURL,
+        });
+      }
 
-        ]
-        for (const file of req.files) {
-            console.log(file)
-            const storageRef = ref(storage, `many_img/${file.originalname + "       " + dateTime}`);
-            const metadata = {
-                contentType: file.mimetype,
-            };
-            const snapshot = await uploadBytesResumable(storageRef, file.buffer, metadata);
-
-            const downloadURL = await getDownloadURL(snapshot.ref);
-            result.push({
-                name: file.originalname,
-                type: file.mimetype,
-                downloadURL: downloadURL
-            })
-        }
-        
-       
-        return res.json({
-            message: 'file uploaded to firebase storage',
-            data: result
-        })
+      return res.json({
+        message: "file uploaded to firebase storage",
+        data: result,
+      });
     } catch (error) {
-        return res.status(400).send(error.message)
+      return res.status(400).send(error.message);
     }
-});
-
+  }
+);
 
 router.post("/", upload.single("filename"), async (req, res) => {
-    try {
-        const dateTime = giveCurrentDateTime();
+  try {
+    const dateTime = giveCurrentDateTime();
 
-        const storageRef = ref(storage, `files/${req.file.originalname + "       " + dateTime}`);
-        console.log("storage",storageRef)
-        // Create file metadata including the content type
-        const metadata = {
-            contentType: req.file.mimetype,
-        };
+    const storageRef = ref(
+      storage,
+      `files/${req.file.originalname + "       " + dateTime}`
+    );
+    // Create file metadata including the content type
+    const metadata = {
+      contentType: req.file.mimetype,
+    };
 
-        // Upload the file in the bucket storage
-        const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
-        //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
-        console.log("print snapshot",snapshot.bytesTransferred, "----", snapshot.totalBytes)
+    // Upload the file in the bucket storage
+    const snapshot = await uploadBytesResumable(
+      storageRef,
+      req.file.buffer,
+      metadata
+    );
+    //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
 
-        // Grab the public url
-        const downloadURL = await getDownloadURL(snapshot.ref);
+    // Grab the public url
+    const downloadURL = await getDownloadURL(snapshot.ref);
 
-        console.log('File successfully uploaded.');
-        return res.json({
-            message: 'file uploaded to firebase storage',
-            name: req.file.originalname,
-            type: req.file.mimetype,
-            downloadURL: downloadURL
-        })
-    } catch (error) {
-        return res.status(400).send(error.message)
-    }
+    return res.json({
+      message: "file uploaded to firebase storage",
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      downloadURL: downloadURL,
+    });
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
 });
 
 const giveCurrentDateTime = () => {
-    const today = new Date();
-    const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    const dateTime = date + ' ' + time;
-    return dateTime;
-}
+  const today = new Date();
+  const date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  const time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  const dateTime = date + " " + time;
+  return dateTime;
+};
 
 module.exports = router;
--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,13 +1,13 @@
-
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 dotenv.config();
-module.exports =  {
-    firebaseConfig:{
-        apiKey: "AIzaSyDkVAyHko-qa_ipXj5nY2mFBkhWrq-xOTE",
-        authDomain: "movie-the-stone.firebaseapp.com",
-        projectId: "movie-the-stone",
-        storageBucket: "movie-the-stone.appspot.com",
-        messagingSenderId: "892766545381",
-        appId: "1:892766545381:web:ee60f433e33f655a4a7b0c"
-    }
-}
+module.exports = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDkVAyHko-qa_ipXj5nY2mFBkhWrq-xOTE",
+    authDomain: "movie-the-stone.firebaseapp.com",
+    projectId: "movie-the-stone",
+    storageBucket: "movie-the-stone.appspot.com",
+    messagingSenderId: "892766545381",
+    appId: "1:892766545381:web:ee60f433e33f655a4a7b0c",
+    measurementId: "G-4YXPLX6QYV",
+  },
+};
