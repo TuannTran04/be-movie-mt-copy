4a6be5377708e6f45800dde69a2ab80417fd5ad6
TuannTran04 <skttfaker25@gmail.com>
2023-10-06 22:50:42 +0700

    add sockettttt l

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.use(helmet());
 const io = require("socket.io")(httpServer, {
   cors: {
     // origin: ["http://localhost:3001", "https://fe-shotflix.vercel.app"],
-    origin: "https://fe-shotflix.vercel.app/",
+    origin: "*",
   },
 });
 global._io = io;
